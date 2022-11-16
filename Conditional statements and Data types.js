a1cb412e438d550dc01a6a5d2a7a6801110430b0
JDramaS <sanches3128@ukr.net>
2022-11-16 15:51:10 +0200

    first task

---
+++ Conditional statements and Data types.js
@@ -0,0 +1,33 @@
+//Perform addition of various types (string + boolean, string + number, number + boolean)
+let a = "5" + true;
+console.log(a);
+
+let b = "5" + 5;
+console.log(b);
+
+let c = 5 + false;
+console.log(c);
+//Perform multiplication of various types (string * boolean, string * number, number * boolean)
+let d = "5" * false;
+console.log(d);
+
+let e = "5" * 5;
+console.log(e);
+
+let f = 5 * true;
+console.log(f);
+
+//Divide different types (string / boolean, string / number, number / Boolean)
+let g = "5" / true;
+console.log(g);
+
+let h = "5" / 5;
+console.log(h);
+
+let i = 5 / false;
+console.log(i);
+//Perform explicit conversion (number, string, boolean)
+
+console.log(Boolean(1));
+console.log(String(6));
+console.log(Number(false));
